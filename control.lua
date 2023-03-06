1c4e0ad21569574a93599000dafe07d79fad80a9
StevenWdV <stevenwdv@gmail.com>
2023-03-06 16:58:00 +0100

    Fixed that despawned biters and biters killed by scripts were still kept track of, increasing memory/storage use

--- control.lua
+++ control.lua
@@ -1,61 +1,65 @@
----Initialize state variables
-local function init_globals()
-    ---@type table<uint,uint64>
-    global.unit_lines = global.unit_lines or {}
-    ---@type table<uint,LuaEntity>
-    global.unit_spawners = global.unit_spawners or {}
-end
+script.on_configuration_changed(function()
+	---@type table<uint,uint64>
+	global.unit_lines = global.unit_lines or {}
+	---@type table<uint,LuaEntity>
+	global.unit_spawners = global.unit_spawners or {}
+
+	-- See issue #1
+	if not global.fixed_leak then
+		for unit_number, unit in pairs(global.unit_spawners) do
+			if unit.valid then
+				script.register_on_entity_destroyed(unit)
+			else
+				global.unit_spawners[unit_number] = nil
+			end
+		end
+		global.fixed_leak = true
+	end
+end)
 
 ---@param player LuaPlayer
 local function clear_line(player)
-    local line = global.unit_lines[player.index]
-    if line then
-        rendering.destroy(line)
-    end
-    global.unit_lines[player.index] = nil
+	local line = global.unit_lines[player.index]
+	if line then
+		rendering.destroy(line)
+	end
+	global.unit_lines[player.index] = nil
 end
 
 script.on_event(defines.events.on_selected_entity_changed,
-    ---@param event on_selected_entity_changed
-    function(event)
-        local player = game.get_player(event.player_index)
-        init_globals()
-        clear_line(player)
-        local unit = player.selected
-        if not unit or unit.type ~= "unit" or not (unit.spawner or
-            global.unit_spawners[unit.unit_number] and global.unit_spawners[unit.unit_number].valid) then
-            return
-        end
-        global.unit_lines[player.index] = rendering.draw_line {
-            surface = unit.surface,
-            from = unit,
-            to = unit.spawner or global.unit_spawners[unit.unit_number],
-            players = { player },
-            only_in_alt_mode = player.mod_settings["rsbh-only-alt-mode"].value,
-            color = { 1, 0, 0 },
-            width = 5,
-        }
-    end)
+	---@param event on_selected_entity_changed
+	function(event)
+		local player = game.get_player(event.player_index)
+		clear_line(player)
+		local unit = player.selected
+		if not unit or unit.type ~= "unit" or not (unit.spawner or
+			global.unit_spawners[unit.unit_number] and global.unit_spawners[unit.unit_number].valid) then
+			return
+		end
+		global.unit_lines[player.index] = rendering.draw_line {
+			surface = unit.surface,
+			from = unit,
+			to = unit.spawner or global.unit_spawners[unit.unit_number],
+			players = { player },
+			only_in_alt_mode = player.mod_settings["rsbh-only-alt-mode"].value,
+			color = { 1, 0, 0 },
+			width = 5,
+		}
+	end)
 
 script.on_event(defines.events.on_entity_spawned,
-    ---@param event on_entity_spawned
-    function(event)
-        local unit = event.entity
-        if unit.type ~= "unit" or not event.spawner then
-            return
-        end
-        init_globals()
-        global.unit_spawners[unit.unit_number] = event.spawner
-    end)
+	---@param event on_entity_spawned
+	function(event)
+		local unit = event.entity
+		if unit.type ~= "unit" or not event.spawner then
+			return
+		end
+		global.unit_spawners[unit.unit_number] = event.spawner
+		script.register_on_entity_destroyed(unit)
+	end)
 
-script.on_event(defines.events.on_entity_died,
-    ---@param event on_entity_died
-    function(event)
-        init_globals()
-        global.unit_spawners[event.entity.unit_number] = nil
-    end, {
-    {
-        filter = "type",
-        type = "unit",
-    }
-})
+script.on_event(defines.events.on_entity_destroyed,
+	---@param event on_entity_destroyed
+	function(event)
+		global.unit_spawners[event.unit_number] = nil
+	end)
